3b3298ad8c0100a704514c7884d85919c8947b7c
BazZziliuS <bazzzil123@gmail.com>
2024-07-20 01:16:51 +0300

    first commit

---
+++ NoBuildingLimit.lua
@@ -0,0 +1,22 @@
+NoBuildingLimit = {}
+
+function NoBuildingLimit:loadMap(name)
+    ProductionChainManager.NUM_MAX_PRODUCTION_POINTS = math.huge
+end
+
+function NoBuildingLimit:deleteMap()
+end
+
+function NoBuildingLimit:keyEvent(unicode, sym, modifier, isDown)
+end
+
+function NoBuildingLimit:mouseEvent(posX, posY, isDown, isUp, button)
+end
+
+function NoBuildingLimit:update(dt)
+end
+
+function NoBuildingLimit:draw()
+end
+
+addModEventListener(NoBuildingLimit)
