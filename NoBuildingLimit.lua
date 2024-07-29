bfae9b2a8b6f607ea3946245953be253e7dbd468
BazZziliuS <bazzzil123@gmail.com>
2024-07-29 16:52:22 +0300

    Support pallet

--- NoBuildingLimit.lua
+++ NoBuildingLimit.lua
@@ -1,8 +1,24 @@
 NoBuildingLimit = {}
 
+function NoBuildingLimit:overwritePalletSpawner()
+    if PalletSpawner ~= nil then
+        function PalletSpawner:spawnPallet(farmId, fillTypeId, callback, callbackTarget)
+            local pallet = self.fillTypeIdToPallet[fillTypeId]
+            if pallet ~= nil then
+                table.insert(self.spawnQueue, {pallet=pallet, fillType=fillTypeId, farmId=farmId, callback=callback, callbackTarget=callbackTarget})
+                g_currentMission:addUpdateable(self)
+            else
+                Logging.devError("PalletSpawner: no pallet for fillTypeId", fillTypeId)
+                callback(callbackTarget, nil, PalletSpawner.NO_PALLET_FOR_FILLTYPE, fillTypeId)
+            end
+        end
+    end
+end
+
 function NoBuildingLimit:loadMap(name)
     ProductionChainManager.NUM_MAX_PRODUCTION_POINTS = math.huge
     HusbandrySystem.GAME_LIMIT = math.huge
+    NoBuildingLimit:overwritePalletSpawner()
 end
 
 function NoBuildingLimit:deleteMap()
