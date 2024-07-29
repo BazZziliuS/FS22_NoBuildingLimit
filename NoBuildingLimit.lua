107afe91e19999a2413ed95de6897783f7bb1587
BazZziliuS <bazzzil123@gmail.com>
2024-07-29 18:43:15 +0300

    Update NoBuildingLimit.lua

--- NoBuildingLimit.lua
+++ NoBuildingLimit.lua
@@ -30,6 +30,7 @@ SlotSystem.NUM_OBJECT_LIMITS = {
 function NoBuildingLimit:loadMap(name)
     ProductionChainManager.NUM_MAX_PRODUCTION_POINTS = math.huge
     HusbandrySystem.GAME_LIMIT = math.huge
+    FillTypeManager.SEND_NUM_BITS = 16.0 -- max 65.536
 end
 
 function NoBuildingLimit:deleteMap()
