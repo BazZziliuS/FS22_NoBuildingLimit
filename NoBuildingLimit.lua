1d43887a22389994039dda8f1988d7c8286082f1
BazZziliuS <bazzzil123@gmail.com>
2024-07-29 18:15:01 +0300

    Update NoBuildingLimit.lua

--- NoBuildingLimit.lua
+++ NoBuildingLimit.lua
@@ -1,34 +1,35 @@
 NoBuildingLimit = {}
 
+SlotSystem.NUM_OBJECT_LIMITS = {
+    [SlotSystem.LIMITED_OBJECT_BALE] = {
+        [PlatformId.WIN] = math.huge,
+        [PlatformId.MAC] = math.huge,
+        [PlatformId.PS5] = math.huge,
+        [PlatformId.PS4] = math.huge,
+        [PlatformId.XBOX_SERIES] = math.huge,
+        [PlatformId.XBOX_ONE] = math.huge,
+        [PlatformId.IOS] = math.huge,
+        [PlatformId.ANDROID] = math.huge,
+        [PlatformId.SWITCH] = math.huge,
+        [PlatformId.GGP] = math.huge
+    },
+    [SlotSystem.LIMITED_OBJECT_PALLET] = {
+        [PlatformId.WIN] = math.huge,
+        [PlatformId.MAC] = math.huge,
+        [PlatformId.PS5] = math.huge,
+        [PlatformId.PS4] = math.huge,
+        [PlatformId.XBOX_SERIES] = math.huge,
+        [PlatformId.XBOX_ONE] = math.huge,
+        [PlatformId.IOS] = math.huge,
+        [PlatformId.ANDROID] = math.huge,
+        [PlatformId.SWITCH] = math.huge,
+        [PlatformId.GGP] = math.huge
+    }
+}	
+
 function NoBuildingLimit:loadMap(name)
     ProductionChainManager.NUM_MAX_PRODUCTION_POINTS = math.huge
     HusbandrySystem.GAME_LIMIT = math.huge
-    SlotSystem.NUM_OBJECT_LIMITS = {
-        [SlotSystem.LIMITED_OBJECT_BALE] = {
-            [PlatformId.WIN] = math.huge,
-            [PlatformId.MAC] = math.huge,
-            [PlatformId.PS5] = math.huge,
-            [PlatformId.PS4] = math.huge,
-            [PlatformId.XBOX_SERIES] = math.huge,
-            [PlatformId.XBOX_ONE] = math.huge,
-            [PlatformId.IOS] = math.huge,
-            [PlatformId.ANDROID] = math.huge,
-            [PlatformId.SWITCH] = math.huge,
-            [PlatformId.GGP] = math.huge
-        },
-        [SlotSystem.LIMITED_OBJECT_PALLET] = {
-            [PlatformId.WIN] = math.huge,
-            [PlatformId.MAC] = math.huge,
-            [PlatformId.PS5] = math.huge,
-            [PlatformId.PS4] = math.huge,
-            [PlatformId.XBOX_SERIES] = math.huge,
-            [PlatformId.XBOX_ONE] = math.huge,
-            [PlatformId.IOS] = math.huge,
-            [PlatformId.ANDROID] = math.huge,
-            [PlatformId.SWITCH] = math.huge,
-            [PlatformId.GGP] = math.huge
-        }
-    }	
 end
 
 function NoBuildingLimit:deleteMap()
